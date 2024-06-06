0c083687c8c0f256978c93f844c4685f8e785eaf
lorainvglezr <149640984+lorainvglezr@users.noreply.github.com>
2024-06-06 03:48:39 -0600

    Platillos.h

--- Platillos.h
+++ Platillos.h
@@ -1,6 +1,6 @@
 // Nombre: Lorain Valeria González Rivas
 // Matrícula: A01707279
-// Fecha: Jueves, 30 de mayo de 2024
+// Fecha: Jueves, 06 de junio de 2024
 // Materia: Programación orientada a objetos (Gpo 304)
 
 /**
